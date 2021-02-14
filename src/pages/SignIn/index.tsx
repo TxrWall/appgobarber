988b11e61680c36cfdcd213e33db6900dac93090
Wallace <wllcblm@gmail.com>
2021-02-14 05:16:35 -0300

    Input e Button components created

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => (
+    <Container>
+        <Image source={logoImg} />
+
+        <Title>Faça seu logon</Title>
+
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+
+        <Button onPress={() => {}}>Entrar</Button>
+    </Container>
+);
+
+export default SignIn;
